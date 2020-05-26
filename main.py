fa032b967f32025acca9e63105c91ee3f6cda9d0
Anthony <cozziad@gmail.com>
2020-05-26 17:46:09 -0400

    blueprint

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from second import second
 
 app = Flask(__name__)
+app.register_blueprint(second,url_prefix="/admin")
 
 @app.route("/home")
 @app.route("/")
-def home():
-    return render_template("home.html")
+def test():
+    return "<h2>test</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True)
